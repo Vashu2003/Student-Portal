4346176f81191f344f1a157f66886f944bba5d35
Vashu2003 <132573798+Vashu2003@users.noreply.github.com>
2025-03-11 14:07:24 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Button, Form, Alert, Container, Row, Col } from "react-bootstrap";
 
 const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("student@gmail.com");
+  const [password, setPassword] = useState("password");
   const [error, setError] = useState(""); // State to handle login errors
 
   const handleLoginSubmit = async (e) => {
